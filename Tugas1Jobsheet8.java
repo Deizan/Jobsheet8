efc39488b50610eb001d6f15eaf0db2cf78d5531
Deizan <deizanfakhri7@gmail.com>
2025-11-01 19:12:08 +0700

    Tugas2

---
+++ Tugas1Jobsheet8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tugas1Jobsheet8 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukkan output angka: ");
+        int N = sc.nextInt();
+
+         System.out.println(N + " " + N + " " + N );
+         System.out.println(N + " " +" "+" " + N );
+         System.out.println(N + " " + N + " " + N );
+
+         System.out.println();
+         
+         System.out.println(N + " " + N + " " + N + " " + N + " " + N );
+         System.out.println(N + "       " + N );
+         System.out.println(N + "       " + N );
+         System.out.println(N + "       " + N );
+         System.out.println(N + " " + N + " " + N + " " + N + " " + N );
+    }
+}
