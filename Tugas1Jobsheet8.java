5f1f7492cf7017bf7ef8004d79fd87c533116123
Deizan <deizanfakhri7@gmail.com>
2025-11-03 04:46:55 +0700

    update tugas 1

--- Tugas1Jobsheet8.java
+++ Tugas1Jobsheet8.java
@@ -1,22 +1,52 @@
 import java.util.Scanner;
-public class Tugas1Jobsheet8{
-    public static void main(String[] args){
+
+public class Tugas1Jobsheet8 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.print("masukkan nilai N (tinggi & lebar): ");
-        int N = sc.nextInt();
-        System.out.print("masukkan angka yang ditampilkan: ");
-        int P = sc.nextInt();
-        
-        for(int i = 1; i <= N;i++){
-            for(int j = 1; j <= N;j++){
-                if (i >=2 && i <= N-1 && j >= 2 && j <= N-1){
-                    System.out.print("  ");
-                    continue;
+        int a, d, n1, n2;
+
+        do {
+            System.out.print("Masukkan nilai n pertama (minimal 3): ");
+            n1 = sc.nextInt();
+        } while (n1 < 3);
+
+        do {
+            System.out.print("Masukkan nilai n kedua (minimal 3): ");
+            n2 = sc.nextInt();
+        } while (n2 < 3);
+
+        int maxRows = Math.max(n1, n2);
+
+        for (a = 1; a <= maxRows; a++) {
+            if (a <= n1) {
+                for (d = 1; d <= n1; d++) {
+                    if (a == 1 || a == n1 || d == 1 || d == n1) {
+                        System.out.print(n1 + " ");
+                    } else {
+                        System.out.print("  ");
                     }
-                System.out.print(" " +P);
+                }
+            } else {
+                for (d = 1; d <= n1; d++) System.out.print("  ");
             }
+
+            System.out.print("  ");
+
+            if (a <= n2) {
+                for (d = 1; d <= n2; d++) {
+                    if (a == 1 || a == n2 || d == 1 || d == n2) {
+                        System.out.print(n2 + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            } else {
+                for (d = 1; d <= n2; d++) System.out.print("  ");
+            }
+
             System.out.println();
         }
+
+        sc.close();
     }
-    }
+}
